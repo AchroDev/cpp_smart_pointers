df58472405254d9d75c9256c9cb2277dc751324b
AchroDev <achrodev@proton.me>
2024-10-06 17:33:24 -0500

    Added example Entity class with constructor and destructor

--- main.cpp
+++ main.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
+#include <string>
+
+// Smart Pointers (Unique, Shared, Weak) in C++
+
+// Example Entity class
+class Entity
+{
+public:
+    Entity()
+    {
+        std::cout << "Created Entity" << std::endl;
+    }
+
+    ~Entity()
+    {
+        std::cout << "Created Entity" << std::endl;
+    }
+};
 
 int main()
 {
