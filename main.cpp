facbd9a9d80c52dc5ef103e8af630710604be71e
AchroDev <achrodev@proton.me>
2024-10-06 17:42:34 -0500

    Added comment about unique pointers and included the memory library header file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <string>
+#include <memory> // To access the smart pointers you will need to include the "memory" library
 
 // Smart Pointers (Unique, Shared, Weak) in C++
+
+/*
+    Unique Pointers have to be unique, you can't copy them, if you did, you would have two unique pointers pointing at the same address in memory
+    and when one of them gets freed, the other would be pointing to memory that's been free'd and will cause a memory leak.
+*/
 
 // Example Entity class
 class Entity
